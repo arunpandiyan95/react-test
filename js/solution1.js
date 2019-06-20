4b4b02ddb86b17970b163887453b3e019f420114
arunpandiyan95 <aruunkrish@gmail.com>
2019-06-20 11:10:06 +0530

    solution1 ans commit

---
+++ js/solution1.js
@@ -0,0 +1,51 @@
+var books = [
+    {
+        "id": 1,
+        "title": "React1",
+        "author": "Prasdeep",
+        "alreadyRead": false
+    },
+    {
+        "id": 2,
+        "title": "React2",
+        "author": "Arun",
+        "alreadyRead": false
+    },
+    {
+        "id": 3,
+        "title": "React3",
+        "author": "Ananth",
+        "alreadyRead": false
+    },
+    {
+        "id": 4,
+        "title": "React4",
+        "author": "Dinesh",
+        "alreadyRead": false
+    }
+];
+
+var bookslist = document.querySelector('#bookslist');
+//bookslist.innerHTML = templateStr;
+
+let appendNode = function(books){
+bookslist.innerHTML = 
+`<ul>
+    ${getlist(books)}
+</ul>`;
+}
+function getlist(){
+    return books.filter(book => book.alreadyRead == false).map(book => `<li>${book.title} by ${book.author}</li><input type="checkbox" class="markread" >`).join('')
+}
+
+appendNode(books);
+
+
+let markreadindexes = document.querySelectorAll('.markread');
+//console.log(markreadindexes)
+markreadindexes.forEach(markreadindex => markreadindex.addEventListener("click", function(event){
+    //console.log(Array.from(markreadindexes).indexOf(event.target));
+    books[Array.from(markreadindexes).indexOf(event.target)].alreadyRead = true;
+    //console.log(books);    
+    appendNode(books);
+}))
