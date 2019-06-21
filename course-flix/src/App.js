57a04b3a7cd47f834c71c52e09ff8d463be11136
arunpandiyan95 <aruunkrish@gmail.com>
2019-06-21 13:30:21 +0530

    working on react-app sample app

---
+++ course-flix/src/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './App.css';
+import Courses from './components/courses';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { 
+      "courses": [
+        { 
+          "id": 1,
+          "course": 'React',
+          "price": 1000,
+          "duration":10,
+          "selected": false
+        },
+        { 
+          "id": 2,
+          "course": 'Angular',
+          "price": 2000,
+          "duration":10,
+          "selected": false
+        },
+        { 
+          "id": 3,
+          "course": 'Java 8',
+          "price": 3000,
+          "duration":10,
+          "selected": false
+        },
+        { 
+          "id": 4,
+          "course": 'Spring',
+          "price": 4000,
+          "duration":20,
+          "selected": false
+        },
+        { 
+          "id": 5,
+          "course": 'Hibernate',
+          "price": 1000,
+          "duration":10,
+          "selected": false
+        },
+        { 
+          "id": 6,
+          "course": 'HTML',
+          "price": 5000,
+          "duration":30,
+          "selected": false
+        }
+      ]
+    }
+
+    this.changeClassName = this.changeClassName.bind(this);
+  }
+
+  changeClassName(e, course) {
+
+    //let newCourse = course;
+
+    //newCourse.selected = true;
+    //console.log(newCourse);
+    
+    //let newArr = this.state.courses.filter(course =>  course.id !== newCourse.id )
+    //this.state.courses[this.state.courses.indexOf(course)].selected = true;
+
+    //newArr.push(newCourse);
+
+    let oldArr = this.state.courses;
+    oldArr[oldArr.indexOf(course)].selected = true
+
+    this.setState({
+      courses: oldArr
+    })
+    console.log(this.state.courses.indexOf(course))
+  }
+
+  render(){
+  return (
+    <div>
+      <Courses data={this.state.courses} handleClick={this.changeClassName} />
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ course-flix/src/components/courses.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Courses extends React.Component{
+
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+    return (
+        <div>
+        {
+         this.props.data.length > 0 ? (    
+         <div className="row">
+            {this.props.data.map(course => 
+
+                <div className="col-md-3" key={course.id} >
+                    <div className="card text-center">
+                    <div className="card-header">
+                        {course.id}
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{course.course}</h5>
+                        <p className="card-text">Duration : {course.duration}</p>
+                        <button course={course} className={course.selected === true ? "btn btn-success" : "btn btn-primary" } onClick={(event) => {this.props.handleClick(event, course)}} >Go somewhere</button>
+                    </div>
+                    <div className="card-footer text-muted">
+                        Price Rs.{course.price}
+                    </div>
+                    </div>
+                </div>                
+
+            )}
+         </div>) : <h1>There are no Courses to display</h1>
+        }
+        </div>
+
+    );
+    }
+}
+
+export default Courses;
